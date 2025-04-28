56d16e0ee7759de3a957980b6d6612a6f9714739
oscar5677 <72199362+oscar5677@users.noreply.github.com>
2025-04-28 17:24:14 +0530

    Update generate_m3u8.py

--- generate_m3u8.py
+++ generate_m3u8.py
@@ -15,7 +15,23 @@ def main():
    # Start the M3U file with a timestamp so that it always changes
     m3u_content = f"#EXTM3U\n# Updated: {datetime.utcnow().isoformat()} UTC\n\n"
 
-    
+    # Add default stream(s) here
+    default_streams = '''
+#EXTINF:-1 tvg-logo=" https://www.fancode.com/skillup-uploads/fc-web-logo/fc_logo_white_bg.svg  " group-title="Cricket",FANCODE
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key=159a2b4e1d2d7f16892d35d935a2fb34:07809840dd0f511221ca78459167d1b3
+https://a166aivottlinear-a.akamaihd.net/OTTB/sin-nitro/live/clients/dash/enc/gdhz5mhnyu/out/v1/fe72171ab2684ab8b9ee3e2ffcc9cff2/cenc.mpd
+
+#EXTINF:-1 tvg-logo=" https://images.fubo.tv/channel-config-ui/station-logos/on-dark/willow_sports_stacked_logo_full_white.png?ch=width&auto=format%2Ccompress&w=0.4&h=0.4" group-title="Cricket",WILLOW SPORTS 
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key=1779c27b9d077a3ba0c9cc1bb9a94b9f:cc5cf3b7928fb9e0a1ee6a8b566f0a8e
+https://abmyxykaaaaaaaamkyvb65fuqebyg.7a77200bf98444ac997a89ed83775793.emt.cf.ww.aiv-cdn.net/iad-nitro/live/clients/dash/enc/f60kqesunw/out/v1/a435ed7a00f947deb4369b46d8f2fb70/cenc.mpd
+
+
+
+
+'''
+    m3u_content += default_streams
 
     group_title = "Willow"  # Fixed group-title to "Willow"
  # group_title = data.get('name', 'Unknown')  # Example: "Willow"
